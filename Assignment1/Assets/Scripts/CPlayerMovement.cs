a53804d3a775b748619f172f0d92dd2baa018c14
ShenRaccoon <www.glennoch@hotmail.com>
2022-01-13 10:57:37 +0800

    a

---
+++ Assignment1/Assets/Scripts/CPlayerMovement.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CPlayerMovement : MonoBehaviour
+{
+    [SerializeField]
+    CharacterController characterController;
+    [SerializeField]
+    Animator animator;
+
+    public float walkSpeed = 1.0f;
+    public float rotationalSpeed = 50.0f;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        float hInput = Input.GetAxis("Horizontal");
+
+        float vInput = Input.GetAxis("Vertical");
+
+
+
+        float speed = walkSpeed;
+
+
+
+        if (Input.GetKey(KeyCode.LeftShift))
+
+        {
+
+            speed = walkSpeed * 2.0f;
+
+        }
+
+
+
+        if (animator == null) return;
+
+
+
+        transform.Rotate(0.0f, hInput * rotationalSpeed * Time.deltaTime, 0.0f);
+
+
+
+        Vector3 forward =
+
+            transform.TransformDirection(Vector3.forward).normalized;
+
+        forward.y = 0.0f;
+
+
+
+        characterController.Move(forward * vInput * speed * Time.deltaTime);
+
+
+
+
+
+        animator.SetFloat("PosX", 0);
+
+        animator.SetFloat("PosZ", vInput * speed / 2.0f * walkSpeed);
+    }
+}
